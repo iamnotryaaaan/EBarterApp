f412e71f79a21aa3ee0238b05d64691d4bd90f9c
envy <marsamoloryan@gmail.com>
2021-10-16 22:21:30 +0800

    first commit

--- app/src/main/java/com/example/loginactivity/LoginActivity.java
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.loginactivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -8,13 +11,20 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
+import org.w3c.dom.Text;
+
 public class LoginActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
     FloatingActionButton Facebook;
     float v=0;
-
+    EditText user;
+    EditText pass;
+    TextView forgetPass;
+    Button login;
+    TextView adminLogin;
+    TextView createAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -23,13 +33,33 @@ public class LoginActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
-        Facebook = findViewById(R.id.fab_Facebook);
+        Facebook = findViewById(R.id.fab_facebook);
+
+        user = findViewById(R.id.user);
+        pass = findViewById(R.id.pass);
+        forgetPass = findViewById(R.id.forget_pass);
+        login = findViewById(R.id.button);
+        adminLogin = findViewById(R.id.adminLogin);
+        createAcc = findViewById(R.id.createAcc);
+
+        user.setTranslationX(800);
+        pass.setTranslationX(800);
+        forgetPass.setTranslationX(800);
+        login.setTranslationX(800);
+        adminLogin.setTranslationX(800);
+        createAcc.setTranslationX(800);
+
+        user.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(300).start();
+        pass.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(300).start();
+        forgetPass.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(300).start();
+        adminLogin.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(300).start();
+        createAcc.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(300).start();
 
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("Signup"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), context: this,tabLayout.getTabCount());
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
